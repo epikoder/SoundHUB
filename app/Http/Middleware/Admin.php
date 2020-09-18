f042a71cb84921be0651612cc0211ac3b4495c6e
eribell <efedua.bell@gmail.com>
2020-09-18 18:59:58 +0100

    Remove unnecessary logic in middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -19,15 +19,6 @@ class Admin
         $user = $request->user();
         if (isset($cookie['X-ADMIN']) && $user)
         {
-            foreach ($user->roles as $role) {
-                if ($role->id == 3) {
-                    $roles['admin'] = true;
-                }
-                if ($role->id == 4) {
-                    $roles['root'] = true;
-                }
-            }
-            $request->roles = $roles;
             if ($cookie['X-ADMIN'] == $user->admins->uuid) {
                 return $next($request);
             }
