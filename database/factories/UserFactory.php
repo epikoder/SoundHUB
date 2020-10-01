753b53553bf0cc8ec71032dcfd425202f7c25b7c
eribell <efedua.bell@gmail.com>
2020-10-01 01:31:43 +0100

    updated seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\Artists;
+use App\Models\EliteArtists;
+use App\Models\Tracks;
 use App\User;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
+use App\Http\Controllers\MediaQuery;
+use App\Models\Albums;
+use App\Models\PlayCount;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +29,39 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => bcrypt('password'), // password
     ];
 });
+$factory->define(Artists::class, function (Faker $faker) {
+    return [
+        'name' => $faker->unique()->userName
+    ];
+});
+$factory->define(EliteArtists::class, function (Faker $faker) {
+    return [
+        'name' => $faker->unique()->firstName()
+    ];
+});
 
+$factory->define(Tracks::class, function (Faker $faker) {
+    $title = $faker->unique()->sentence(rand(2, 3));
+    return [
+        'title' => $title,
+        'slug' => MediaQuery::slugUnique($title, '\App\Models\Tracks'),
+        'genre' => 'blues',
+        'url' => 'url',
+    ];
+});
+
+$factory->define(Albums::class, function (Faker $faker) {
+    $title = $faker->unique()->sentence(rand(2,3));
+    return [
+        'title' => $title,
+        'slug' => MediaQuery::slugUnique($title, '\App\Models\Albums'),
+        'genre' => 'rap',
+        'track_num' => rand(3,24)
+    ];
+});
+
+$factory->define(\App\Models\PlayCount::class, function () {
+    return [
+        'count' => (rand() % rand()) % rand()
+    ];
+});
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,17 +1,71 @@
 <?php
 
+use App\Models\Albums;
+use App\Models\Artists;
+use App\Models\EliteArtists;
+use App\Models\Tracks;
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     * @param Faker $faker
      * @return void
      */
     public function run()
     {
-        factory(User::class, 20)->create();
+        factory(User::class, 10)->create()->each(function ($user) {
+            $user->artists()->save(factory(Artists::class)->make())->each(function ($a) {
+                /*
+                $a->tracks()->createMany(factory(Tracks::class, 12)->make([
+                    'artist' => $a->name
+                ])->toArray())->each(function ($track) {
+                    $track->playcount()->save(factory(\App\Models\PlayCount::class)->make([
+                        'title' => $track->title
+                    ]));
+                });
+                $a->albums()->createMany(factory(Albums::class, 7)->make([
+                    'artist' => $a->name
+                ])->toArray())->each(function ($album) {
+                    $album->tracks()->createMany(factory(Tracks::class, $album->track_num)->make([
+                        'artist' => $album->artist
+                    ])->toArray())->each(function ($track) {
+                        $track->playcount()->save(factory(\App\Models\PlayCount::class)->make([
+                            'title' => $track->title
+                        ]));
+                    });
+                });
+                */
+            });
+        });
+
+        factory(User::class, 10)->create()->each(function ($user) {
+            $user->eliteArtists()->save(factory(EliteArtists::class)->make())->each(function ($a) {
+                /*
+                $a->tracks()->createMany(factory(Tracks::class, 12)->make([
+                    'artist' => $a->name
+                    ])->toArray())->each(function ($track) {
+                    $track->playcount()->save(factory(\App\Models\PlayCount::class)->make([
+                        'title' => $track->title
+                    ]));
+                });
+                $a->albums()->createMany(factory(Albums::class, 7)->make([
+                    'artist' => $a->name
+                    ])->toArray())->each(function ($album) {
+                        $album->tracks()->createMany(factory(Tracks::class, $album->track_num)->make([
+                            'artist' => $album->artist
+                        ])->toArray())->each(function ($track) {
+                            $track->playcount()->save(factory(\App\Models\PlayCount::class)->make([
+                                'title' => $track->title
+                            ]));
+                        });
+                    });
+                    */
+            });
+        });
     }
 }
